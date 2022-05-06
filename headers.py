50104d757f49caf057e0d8809342529ef7e6f26e
Antonio González <antgon@cantab.net>
2022-05-06 21:20:05 +0100

    Remove unnecessary import

--- headers.py
+++ headers.py
@@ -19,7 +19,6 @@ You should have received a copy of the GNU General Public License along
 with edfrw. If not, see <http://www.gnu.org/licenses/>.
 """
 
-import struct
 import warnings
 import datetime as dt
 import numpy as np
